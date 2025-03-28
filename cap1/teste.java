394da5a461807d2ad15cbf4a6b2a40abf887a106
Sophia <sophialimasantos4@gmail.com>
2025-03-28 13:22:41 -0300

    cap 1

---
+++ cap1/teste.java
@@ -0,0 +1,8 @@
+public class teste {
+    /*o nome da classe tem que ser o mesmo do arquivo */
+    public static void main (String[] args) {
+
+        System.out.println("Amo minha princesa!");
+
+    }
+}
