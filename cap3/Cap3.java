f235a8ea87d8b052da5566e8e39b2ebe8ba8bab3
Sophia Lima <sophialimasantos4@gmail.com>
2025-04-01 14:59:53 -0300

    fazendo cap 3

---
+++ cap3/Cap3.java
@@ -0,0 +1,21 @@
+public class Cap3 {
+
+    public static void main(String[] args) {
+        var a = !(false);
+
+        if (a) System.out.println("Verdadeiro");
+        else System.out.println("Falso");
+
+    /* A)Prova semestral que compõe 50% da média;
+    B) trabalho de conclusão de curso que possui 30%;
+    C) as avaliações intermediárias, que são compostas por duas avaliações durante o semestre e a média dessas duas avaliações contribuem com 20% para a média final.
+     */
+    }
+
+    float ps = 7, tcc = 5, av1 = 10, av2 = 6;
+    double mediaFinal = ps * 0.5 + tcc * 0.3 + ((av1 + a2) /2) * 0.2;
+    System.out.println("A média final é: " + mediaFinal);
+
+    if(mediaFinal > 6) System.out.println("Está aprovado.")
+    else System.out.println("Não está aprovado.")
+}
